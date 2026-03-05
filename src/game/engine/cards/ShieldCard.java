ddacdee1f83e9bdbb47db2943028a688d4ab977d
mohamedserag203-byte <mohamedserag203@gmail.com>
2026-03-05 21:10:10 +0200

    Create ShieldCard.java

---
+++ src/game/engine/cards/ShieldCard.java
@@ -0,0 +1,8 @@
+package game.engine.cards;
+
+public class ShieldCard extends Card{
+	ShieldCard(String name, String description, int rarity,boolean lucky){
+		super(name,description,rarity,true);
+		
+	}
+}
