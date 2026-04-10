d2decdffe32f52ba57b45995444ed8775b6cdae8
Amr Walid <45449863+amrwlaid@users.noreply.github.com>
2026-04-10 16:53:25 +0200

    Add files via upload

--- src/game/engine/monsters/Schemer.java
+++ src/game/engine/monsters/Schemer.java
@@ -1,9 +1,11 @@
-// monsters package
 package game.engine.monsters;
-import game.engine.Role;
-public class Schemer extends Monster{
-public Schemer(String name, String description, Role role, int energy){
-	super( name,  description,  role,  energy);	
-}
-}
 
+import game.engine.Role;
+
+public class Schemer extends Monster {
+	
+	public Schemer(String name, String description, Role role, int energy) {
+		super(name, description, role, energy);
+	}
+	
+}
