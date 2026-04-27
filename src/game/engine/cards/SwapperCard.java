6ffcf8620201520c66b69106d21a22aafe756be3
mohamedserag203-byte <mohamedserag203@gmail.com>
2026-04-27 21:19:26 +0300

    Update SwapperCard.java

--- src/game/engine/cards/SwapperCard.java
+++ src/game/engine/cards/SwapperCard.java
@@ -5,10 +5,8 @@ public class SwapperCard extends Card {
 	public SwapperCard(String name, String description, int rarity) {
 		super(name, description, rarity, true);
 	}
-	public void performAction(Monster player, Monster opponent)
-	{
-		if (player.getPosition() < opponent.getPosition())
-		{
+	public void performAction(Monster player, Monster opponent){
+		if (player.getPosition() < opponent.getPosition()){
 			int temp = player.getPosition();
 			player.setPosition(opponent.getPosition());
 			opponent.setPosition(temp);
